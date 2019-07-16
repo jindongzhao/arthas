78ae8db44d3c78d3a79d5f9bcfd8d7ea1fd98c08
zhaojindong <jindongzhao@126.com>
2019-07-16 19:54:01 +0800

    添加事务

--- manage-server/src/main/java/com/taobao/arthas/manage/dao/AppConnectionDao.java
+++ manage-server/src/main/java/com/taobao/arthas/manage/dao/AppConnectionDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.taobao.arthas.manage.dao.domain.AppConnectionDo;
 
@@ -13,6 +14,7 @@ public interface AppConnectionDao extends JpaRepository<AppConnectionDo, Long> {
 	AppConnectionDo getByAppipAndPid(@Param("appIp") String appIp, @Param("pid") Integer pid);
 	
 
+	@Transactional
 	@Modifying
 	@Query("update AppConnectionDo set lastConnectionTime = :time where id = :id")
 	void updateConnectionTime( @Param("id") Long id,  @Param("time") Long time);
