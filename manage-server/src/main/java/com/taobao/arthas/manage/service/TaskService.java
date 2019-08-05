fa2001de1035f235af7d637212ab44ba9998622d
zhaojindong <jindongzhao@126.com>
2019-08-05 19:58:48 +0800

    log

--- manage-server/src/main/java/com/taobao/arthas/manage/service/TaskService.java
+++ manage-server/src/main/java/com/taobao/arthas/manage/service/TaskService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import com.alibaba.fastjson.JSON;
 import com.taobao.arthas.manage.client.ICommandSender;
 import com.taobao.arthas.manage.client.TelnetCommandSender;
 import com.taobao.arthas.manage.constants.enums.TaskStatusEnum;
@@ -66,7 +67,7 @@ public class TaskService {
 					public void run() {
 						// 发送命令行
 						ICommandSender commandSender = new TelnetCommandSender(appClientDo.getAppIp(), appClientDo.getCmdTelnetPort());
-						logger.info("发送命令:{}",taskDo.getCmd());
+						logger.info("发送命令,appClientDo:{}",JSON.toJSONString(appClientDo));
 						String cmdResult = commandSender.sendCommand(taskDo.getCmd());
 						logger.info("接收到命令执行结果:{}",cmdResult);
 						
