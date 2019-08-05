040c1bbf887fa903aec7837ccb352f036dfd3b28
zhaojindong <jindongzhao@126.com>
2019-08-05 17:33:49 +0800

    修改manage server端口号

--- boot/src/main/java/com/taobao/arthas/boot/GlobalConfig.java
+++ boot/src/main/java/com/taobao/arthas/boot/GlobalConfig.java
@@ -8,7 +8,7 @@ public class GlobalConfig {
 	public static  final int DEFAULT_HTTP_PORT = 8563;
 	public static  final long DEFAULT_SESSION_TIMEOUT_SECONDS = ShellServerOptions.DEFAULT_SESSION_TIMEOUT / 1000;
 	
-	public static final String MANAGE_SERVER_URL_BASE = "http://127.0.0.1:8080";
+	public static final String MANAGE_SERVER_URL_BASE = "http://127.0.0.1:9009";
 
 	public static  final String JAR_URL_BASE = "https://github.com/jindongzhao/arthas/blob/zeus/lib/3.1.1/";
 	public static  final String JAR_URL_AGENT = JAR_URL_BASE + "arthas-agent-3.1.1.jar?raw=true";
